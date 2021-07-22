d90142489365e74523a8b47e1fd4c060e7575520
Marcel Roxha <marcel.augusto.roxha@gmail.com>
2021-07-21 21:20:36 -0300

    Projeto - Adm Page Config

--- adminPage.php
+++ adminPage.php
@@ -4,10 +4,10 @@ session_start();
 
  * 
  *  */
-if (!isset($_SESSION['username'])) {
-    header("location: index.php");
-} else {
+if (isset($_SESSION['username'])) {
     $usuario = $_SESSION['username'];
+} else {
+    header("location: index.php");
 }
 //unset($_SESSION['username']);
 ?>
@@ -28,63 +28,78 @@ and open the template in the editor.
     <body id="corpo-adminPage">        
     <center>
         <div id="imgmysql">
-                    <img src="./imagens/logo_padrao_oficial.png" width="446">
-                </div>
-        
+            <img src="./imagens/logo_padrao_oficial.png" width="446">
+        </div>
+
     </center>
-                
-        <nav class="logo">
-
-            
-
-                <ul>
-                    <li>
-                        <div id="imgboostrap">
-                            <a href="https://getbootstrap.com/" target="_blank">
-                                <img src="./imagens/icone_booststrap.png" width="100">
-                            </a>
-                            
-                        </div>
-                    </li>
-                    <li>
-                        <div id="imgfirebase">
-                            <a href="https://firebase.google.com/"target="_blank">
-                            <img src="./imagens/icone_firebase.png" width="250">
-                            </a>
-                        </div>
-
-                    </li>
-
-                    <li>
-                        <div id="imgmysql">
-                            
-                            <a href="https://www.mysql.com/"target="_blank"> 
-                            <img src="./imagens/icone_mysql.png" width="250">
-                            </a>
-                        </div>
-                    </li>
 
 
-                    <li>
-                        <div id="imgmysql">
-                            
-                            <a href="https://github.com/"target="_blank">
-                             <img src="./imagens/GitHub-Logo.png" width="250">
-                            </a>
-                           
-                        </div>
-                    </li>
+    <nav id="logo"><!-- Nav dos elementos de icones -->
 
-                </ul>
 
-        </nav>
-        
+
+        <ul>
+            <li>
+                <div id="imgboostrap">
+                    <a id="iconbootstrap" href="https://getbootstrap.com/" target="_blank">
+                        <img src="./imagens/icone_booststrap.png" width="100">
+                    </a>
+
+                </div>
+            </li>
+            <li>
+                <div id="imgfirebase">
+                    <a id="iconfirebase" href="https://firebase.google.com/"target="_blank">
+                        <img src="./imagens/icone_firebase.png" width="250">
+                    </a>
+                </div>
+
+            </li>
+
+            <li>
+                <div id="imgmysql">
+
+                    <a id="iconmysql" href="https://www.mysql.com/"target="_blank"> 
+                        <img src="./imagens/icone_mysql.png" width="170">
+                    </a>
+                </div>
+            </li>
+
+
+            <li>
+                <div id="imgmysql">
+
+                    <a id="icongithub" href="https://github.com/"target="_blank">
+                        <img src="./imagens/GitHub-Logo.png" width="170">
+                    </a>
+
+                </div>
+            </li>
+
+        </ul>
+
+    </nav><!-- Nav dos elementos de icones -->
+
+    <input type="checkbox" id="chec">
+    <label for="chec">            
+        <img src="./imagens/icon_menu.png" width="150">
+    </label>
+    <nav id="menu">
+        <ul id="ulmenu">
+            <li><a id="itensmenu" href="">Inicio</a></li>
+            <li><a id="itensmenu" href="">Contato</a></li>
+            <li><a id="itensmenu" href="">Serviços</a></li>
+            <li><a id="itensmenu" href="">Missão</a></li>
+            <li><a id="itensmenu" href="">Visão</a></li>            
+        </ul>
+    </nav>
+
     <center>
         <br><br>
         <div>Bem vindo <?php echo "$usuario" ?></div>
     </center>
-        
-         
+
+
 
 </body>
 </html>
